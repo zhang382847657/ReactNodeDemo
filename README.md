72552f8250047b132793354034ca105e2077cb2d
为毛哩 <382847657@qq.com>
2018-02-05 18:14:58 +0800

    -antd-mobile 集成好了

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 ## 运行步骤
 1. `npm install`  下载依赖
 2. `npm run build` 编译js
-3. `npm run server`   --启动服务指向index.html
+3. `npm run server`   --启动服n务指向index.html
--- app/Greeter.js
+++ app/Greeter.js
@@ -5,12 +5,15 @@
 import React, {Component} from "react";
 import config from "./config.json";
 import styles from './Greeter.css';
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 
 export default class Greeter extends Component {
     render() {
         return (
             <div className={styles.root}>
                 {config.greetText}
+                <Button>default</Button>
+                <WhiteSpace />
                 <div className = {styles.test}>3333</div>
             </div>
 
